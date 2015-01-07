1b352492dba34263a8197e9f8124795d591157c9
Mark Bellaire <mark.bellaire@gmail.com>
2015-01-07 09:29:25 -0500

    Customize PHP to desired format
    Changed the indentation to 2 spaces and made the example step to be
    smaller and more concise.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -17,29 +17,25 @@ use Behat\Gherkin\Node\PyStringNode,
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
-{
-    /**
-     * Initializes context.
-     * Every scenario gets its own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
-    }
+class FeatureContext extends BehatContext {
+  /**
+   * Initializes context.
+   * Every scenario gets its own context object.
+   *
+   * @param array $parameters context parameters (set them up through behat.yml)
+   */
+  public function __construct(array $parameters) {
+    // Initialize your context here
+  }
 
-//
-// Place your definition and hook methods here:
-//
-//  /**
-//   * @Given /^I have done something with "([^"]*)"$/
-//   */
-//  public function iHaveDoneSomethingWith($argument) {
-//    doSomethingWith($argument);
-//  }
-//
+  // Place your definition and hook methods here:
+  //
+  ///**
+  // * @Given /^I have done something with "([^"]*)"$/
+  // */
+  //public function iHaveDoneSomethingWith($argument) {
+  //  doSomethingWith($argument);
+  //}
 
   /**
    * @Given /^PHPExcel is installed$/
